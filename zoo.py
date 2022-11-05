7799de266f316efc1fd7ab7f8f9a83c38764122b
Crystal Korth <101788833+defworm@users.noreply.github.com>
2022-11-05 13:44:04 -0500

    all lines printing as intended

--- zoo.py
+++ zoo.py
@@ -6,6 +6,32 @@ class Person:
   def get_name(self):
     return self.name
 
+class Customer(Person):
+    def __init__(self, in_name, in_age):
+        super().__init__(in_name, in_age)
+        self.hasTicket = False
+        self.inZoo = False
+
+    def buy_ticket(self):
+        if self.age < 18:
+            print(f"{self.name} purchased a youth ticket")
+        else:
+            print(f"{self.name} purchased an adult ticket")
+        self.hasTicket= True
+
+    def enter_zoo(self, zoo):
+        if self.hasTicket:
+            zoo.add_customer (self.name)
+            self.hasTicket = False
+            self.inZoo = True
+        else:
+            print("Ticket needed to enter the zoo please")
+
+    def exit_zoo(self, zoo):
+        if self.inZoo:
+            self.inZoo = False
+            zoo.remove_customer(self.name)
+
 class Zoo:
   def __init__(self,name="Local Zoo"):
     self.name = name
@@ -43,6 +69,30 @@ class Animal:
     print("Every animal makes noise")
   def eat_food(self):
     print("All creatures need sustenance")
+
+class Fish(Animal):
+    def __init__(self, name):
+        super().__init__(name)
+    def make_noise(self):
+        print(f"{self.name} says fsh fsh fsh")
+    def eat_food(self):
+        print(f"{self.name} eat worms")
+
+class Bird(Animal):
+    def __init__(self, name):
+        super().__init__(name)
+    def make_noise(self):
+        print(f"{self.name} goes squawk squawk squawk")
+    def eat_food(self):
+        print(f"{self.name} eats termites")
+
+class Chimp(Animal):
+    def __init__(self, name):
+        super().__init__(name)
+    def make_noise(self):
+        print(f"{self.name} goes ooh ooh ahh ahh")
+    def eat_food(self):
+        print(f"{self.name} eats plantains")
 
 nycZoo = Zoo("NYC Zoo")
 
